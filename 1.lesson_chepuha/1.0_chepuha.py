b6b30384e2ccf5baa8399bd45d603c5ddd60708d
kaminin1986 <kaminin-a@mail.ru>
2021-08-04 18:58:37 +0300

    доработал не сохранияла

--- 1.lesson_chepuha/1.0_chepuha.py
+++ 1.lesson_chepuha/1.0_chepuha.py
@@ -4,4 +4,4 @@ verb = input ('Введите глагол в прошедшем времени:
 animal = input ('Ввидите животное: ')
 linin = input ('Характеристика животного: ')
 print ('Ваша чепуха')
-print ('Этот', adjective, noun, verb, 'на ленивую', linin, animal. )
+# print ('Этот', adjective, noun, verb, 'на ленивую', linin, animal )
