4e6b626a6e728e34603bb105237451743d8ed35c
kaminin1986 <kaminin-a@mail.ru>
2021-08-05 09:54:28 +0300

    234

--- 1.1_SquareSpiral1.py
+++ 1.1_SquareSpiral1.py
@@ -2,7 +2,6 @@
 # https://docs.python.org/3/library/turtle.html?highlight=turtle#module-turtle
 import turtle
 t = turtle.Pen() # всроиный модуль turtle
-
 for x  in range(100):
     t.forward(x)
     t.left(90)
--- 12.py
+++ 12.py
@@ -2,3 +2,4 @@ import turtle
 t = turtle.Pen() 
 t.forward(50)
 t.left(90)
+
