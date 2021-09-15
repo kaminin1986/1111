47ed59b5fad0d5d593e19260b0fb1d8636320a88
kaminin1986 <kaminin-a@mail.ru>
2021-09-15 17:30:12 +0300

    123

--- 3.stroka/3.2stroka_v_okne.py
+++ 3.stroka/3.2stroka_v_okne.py
@@ -1,3 +1,18 @@
-import turtle 
+import turtle # Установка графики Turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+colors = ["red", "yellow", "blue", "green"]
+
+#Запросить имя пользователя с помощью всплывающего окна
+# textinput
 i = turtle.textinput("Окно запроса имени", "Как твое имя?")
+
+#нарисовать спираль из имени повторяемого 100 раз
+for x in range(100):
+    t.pencolor (colors[x%4])#по очереди выбрать 4 цвета
+    t.penup() # Не ресовать обычные линии спирали
+    t.forward(x*4) # Переместить черепашку по экрану
+    t.pendown()
+    t.write(i, font= ("Arial", int( (x+4)/4), "bold"))
+    t.left(92)
 print (i)
